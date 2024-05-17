8c2105beea2c55771545fa9cc25e63f63b6dc2fd
Levis <lfonsoh@terpmail.umd.edu>
2024-05-17 05:37:10 -0400

    Changes: Connect the about page to our local API. The buttons can now pull data from our API; Add voice command functionality to all the pages.

--- ExpressProjectApp/app.js
+++ ExpressProjectApp/app.js
@@ -2,13 +2,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
+    res.sendFile('public/377-final-homePage.html', {root: __dirname})
+})
+
+app.get('/mission', (req, res) => {
+    res.header('Content-Type', 'application/json')
+    var word = {
+        'mission':'Our mission is to have weather information be ready and easily accessible for University of Maryland, College Park students.'
+    }
+    res.send(JSON.stringify(word))
+})
+
+app.get('/values', (req, res) => {
+    res.header('Content-Type', 'application/json')
+    var word = {
+        'Accessibility':"Accessibility: We made sure this product is inclusive by design, with built-in accessibility features to make it easy for our users to use"
+    }
+    res.send(JSON.stringify(word))
+})
+
+app.get('/team', (req, res) => {
     res.header('Content-Type', 'application/json')
     var word = {
         'hi':'Hello'
     }
     res.send(JSON.stringify(word))
 })
+
+
 
 app.listen(port, () => {
     console.log(`Express app Listing on port ${port}`)
