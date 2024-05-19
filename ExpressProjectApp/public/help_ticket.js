c5f44114c827add79e428f6e5659eee6e6b1c3b1
danielvillanoh <danielvillano@gmail.com>
2024-05-19 16:53:54 -0400

    Display users ticket and gives the option view previous ticket.

--- ExpressProjectApp/public/help_ticket.js
+++ ExpressProjectApp/public/help_ticket.js
@@ -1,7 +1,6 @@
-//Testing to see ticket data is loaded 
-
 var host = window.location.origin;
 console.log(host)
+
 
 async function loadTicketData() {
     await fetch(`${host}/ticket_data`)
@@ -9,6 +8,50 @@ async function loadTicketData() {
     .then(res => {
         console.log(res)
 
+        const ticket_receipt = document.getElementById('ticket-receipt');
+        ticket_receipt.innerHTML = ''
+
+        //When ticket is submitted, the receipt for the current ticket is shown 
+        if (res.length > 0){
+            const currentTicket = res[0]
+            const currentTicketElement = document.createElement('div')
+            currentTicketElement.className = 'ticket';
+
+            currentTicketElement.innerHTML = `
+            <h2>Current Ticket</h2>
+            <p>On ${currentTicket.date}, <strong>${currentTicket.name}</strong> wrote: </p>
+            <br>
+            <p><strong>${currentTicket.description}</strong></p>
+            `;
+            ticket_receipt.appendChild(currentTicketElement);
+
+        //Gives users the option to view previous ticket without previous user name. 
+            if (res.length > 1){
+                const prevTicket = res[1];
+                const prevTicketElement = document.createElement('div');
+
+                prevTicketElement.className = 'ticket';
+                prevTicketElement.style.display = 'none';
+                prevTicketElement.innerHTML = `
+                <h2>Previous Ticket</h2>
+                <p>On ${prevTicket.date}, <strong>Anonymous</strong> wrote: </p>
+                <br>
+                <p><strong>${prevTicket.description}</strong></p>
+                `;
+                ticket_receipt.appendChild(prevTicketElement);
+
+                const prevButton = document.createElement('button');
+                prevButton.textContent = 'Show Previous Ticket';
+
+                prevButton.onclick = function(){
+                    currentTicketElement.style.display = 'none';
+                    prevTicketElement.style.display = 'block';
+                    prevButton.style.display = 'none';
+
+                };
+                ticket_receipt.appendChild(prevButton);
+            }
+        }
     })
 }
 
@@ -28,7 +71,7 @@ async function createTicket() {
     .then((res) => res.json())
     .then((res) =>{
     })
+    alert("Ticket Submitted Successfully! Your receipt will be displayed.");
     await loadTicketData();
-}
 
-window.onload = loadTicketData;
+}
